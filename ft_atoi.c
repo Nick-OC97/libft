57dc01a0a26a75564d2d3871983045d2a867b30d
Nicholas O'CONNELL <no-conne@c4r16s8.wethinkcode.co.za>
2019-05-20 11:17:53 +0200

    added gitignore and fixed atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: no-conne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 08:19:12 by no-conne          #+#    #+#             */
-/*   Updated: 2019/05/20 09:26:04 by no-conne         ###   ########.fr       */
+/*   Updated: 2019/05/20 11:12:47 by no-conne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,12 @@ int		ft_atoi(char *str)
 	num = 0;
 	if (!str)
 		return (0);
-	while (str[i] == ' ' || str[i] == '\v' || str[i] == '\n' || str[i] == '\r' || str[i] == '\f' || str[i] == '\t' || (str[i] < 48 || str[i] > 57))
-		i++;
-	if (str[i] == '-' && (str[i+1] >= 48 && str[i+1] <= 57)) //check if negative, with dash and next charcater a number
+	while (str[i] && (str[i] < 48 || str[i] > 57))
 	{
-		sign = -1;
+		if (str[i] == '-' && (str[i+1] >= 48 && str[i+1] <= 57))
+		{
+			sign = -1;
+		}	
 		i++;
 	}
 	while (str[i] && str[i] >= 48 && str[i] <= 57)
