4f06dcfaeaad050d7de388ce757a0aac295e1e49
Nicholas O'CONNELL <no-conne@c4r16s11.wethinkcode.co.za>
2019-05-22 14:05:49 +0200

    finished atoi tweaks

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,20 @@
 /*   By: no-conne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 08:19:12 by no-conne          #+#    #+#             */
-/*   Updated: 2019/05/21 11:12:33 by no-conne         ###   ########.fr       */
+/*   Updated: 2019/05/22 14:04:47 by no-conne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_atoi(const char *str)
+static int	checker(const char *str, int i)
+{
+	if (str[i] == '-' && (str[i + 1] == '+' || str[i + 1] == '-'))
+		return (0);
+	if (str[i] == '+' && (str[i + 1] == '-' || str[i + 1] == '+'))
+		return (0);
+	return (1);
+}
+
+int			ft_atoi(const char *str)
 {
 	int i;
 	int num;
@@ -19,16 +28,17 @@ int		ft_atoi(const char *str)
 	i = 0;
 	sign = 1;
 	num = 0;
-	if (!str)
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (checker(str, i) == 0)
 		return (0);
-	while (str[i] && (str[i] < 48 || str[i] > 57))
+	while (str[i] == '-')
 	{
-		if (str[i] == '-' && (str[i + 1] >= 48 && str[i + 1] <= 57))
-		{
-			sign = -1;
-		}
+		sign = -1;
 		i++;
 	}
+	if (str[i] == '+' && str[i + 1] != '-')
+		i++;
 	while (str[i] && str[i] >= 48 && str[i] <= 57)
 	{
 		num = num * 10 + (str[i] - 48);
