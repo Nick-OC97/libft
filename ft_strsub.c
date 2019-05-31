70b3299ed807465913b5fe784811b4fdfbb5edce
Nicholas O'CONNELL <no-conne@c4r16s11.wethinkcode.co.za>
2019-05-31 11:04:29 +0200

    fixed things

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: no-conne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 10:19:55 by no-conne          #+#    #+#             */
-/*   Updated: 2019/05/29 10:43:09 by no-conne         ###   ########.fr       */
+/*   Updated: 2019/05/31 09:44:29 by no-conne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	size_t	i;
 
 	i = 0;
-	if (!(new = (char *)malloc(len)))
+	if (!(new = (char *)malloc(len + 1)))
 		return (NULL);
 	while (s[start] != '\0' && i < len)
 	{
