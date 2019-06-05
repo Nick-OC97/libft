fbf99190c9ec8d15244a435816f6ad0ff028797a
Nicholas O'CONNELL <no-conne@c4r14s6.wethinkcode.co.za>
2019-06-05 12:54:25 +0200

    error fixing

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: no-conne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 10:31:01 by no-conne          #+#    #+#             */
-/*   Updated: 2019/06/05 12:44:35 by no-conne         ###   ########.fr       */
+/*   Updated: 2019/06/05 12:49:09 by no-conne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
 	del((*alst)->content, (*alst)->content_size);
 	if ((*alst)->next != NULL)
-		ft_lstdel(&((*alst)->next), (*del));	
+		ft_lstdel(&((*alst)->next), (*del));
 	free(*alst);
 	*alst = NULL;
 }
--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: no-conne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 10:15:28 by no-conne          #+#    #+#             */
-/*   Updated: 2019/06/05 10:50:34 by no-conne         ###   ########.fr       */
+/*   Updated: 2019/06/05 12:51:07 by no-conne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
-	del((*alst)->content, (*alst)->content_size);
-	free(*alst);
-	*alst = NULL;
+	if (alst && del)
+	{
+		del((*alst)->content, (*alst)->content_size);
+		free(*alst);
+		*alst = NULL;
+	}
 }
