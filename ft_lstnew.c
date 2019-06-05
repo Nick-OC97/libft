bce4158ca858a88d91f1f4c31f359ed9d6c51062
Nicholas O'CONNELL <no-conne@c4r14s6.wethinkcode.co.za>
2019-06-05 09:42:14 +0200

    aded lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: no-conne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 13:56:40 by no-conne          #+#    #+#             */
-/*   Updated: 2019/06/04 14:02:50 by no-conne         ###   ########.fr       */
+/*   Updated: 2019/06/05 09:24:22 by no-conne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,25 @@
 
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
+	t_list	*list;
 
+	if (!(list = (t_list *)malloc(sizeof(t_list))))
+		return (NULL);
+	if (list == NULL)
+		return (NULL);
+	if (content == NULL)
+	{
+		list->content = NULL;
+		list->content_size = 0;
+	}
+	else
+	{
+		list->content = malloc(sizeof(content));
+		if (list->content == NULL)
+			return (NULL);
+		ft_memcpy((list->content), content, sizeof(content));
+		list->content_size = content_size;
+	}
+	list->next = NULL;
+	return (list);
 }
